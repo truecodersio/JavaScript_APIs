2001893ec5896edbfb439212b765d8029bf2e3ce
Tanner Riddle <Tanner.drayton01@gmail.com>
2021-03-09 15:22:30 -0600

    answer:

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 console.log("Hello World!\n==========\n");
+const apiKey = "yrvFAIrfAQUlf1dZYqSLicPFW5ugmcHH";
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let submit = document.getElementById("Submit").addEventListener("click", submitGif);
+
+function submitGif() {
+	let gif = document.getElementById("gif");
+	let gifSearch = document.getElementById("gifSearch").value;
+
+	fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${gifSearch}`, {
+		mode: "cors",
+	})
+		.then(function (response) {
+			console.log(response);
+			return response.json();
+		})
+		.then(function (response) {
+			console.log(response);
+			return (gif.src = response.data.images.original.url);
+		})
+		.catch(function (reject) {
+			console.log(reject);
+		});
+}
